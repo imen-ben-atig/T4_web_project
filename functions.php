7cbfd276c76ae90a07b0aa94073fdaa428d183dc
YosriElhadj <80210302+YosriElhadj@users.noreply.github.com>
2022-05-09 22:57:06 +0100

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+function pdo_connect_mysql() {
+    $DATABASE_HOST = 'localhost';
+    $DATABASE_USER = 'root';
+    $DATABASE_PASS = '';
+    $DATABASE_NAME = 'projet';
+    try {
+    	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
+    } catch (PDOException $exception) {
+    	// If there is an error with the connection, stop the script and display the error.
+    	exit('Failed to connect to database!');
+    }
+}
+
+
+?>
